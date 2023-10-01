16f879d732242610aaac31db5281c7c3618cbcf2
rhick0516 <145033848+rhick0516@users.noreply.github.com>
2023-10-01 15:47:20 +0800

    Add files via upload

---
+++ Program/index.php
@@ -0,0 +1,387 @@
+<?php
+session_start();
+if (!isset($_SESSION["user"])) {
+   header("Location: login.php");
+
+   $errors = array();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <title>AJAX-PHP-CRUD</title>
+
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"/>
+</head>
+<body>
+<div class="container">
+        <h1>Welcome to Dashboard</h1>
+        <a href="logout.php" class="btn btn-warning">Logout</a>
+    </div>
+
+<div class="modal fade" id="studentAddModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Add Student</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <form id="saveStudent">
+            <div class="modal-body">
+
+                <div id="errorMessage" class="alert alert-warning d-none"></div>
+
+                <div class="mb-3">
+                    <label for="">full name</label>
+                    <input type="text" Name="name" class="form-control"/>
+                </div>
+                <div class="mb-3">
+                    <label for="">Email</label>
+                    <input type="text" name="email" class="form-control"/>
+                </div>
+                <div class="mb-3">
+                    <label for="">Phone</label>
+                    <input type="text" name="phone" class="form-control"/>
+                </div>
+                <div class="mb-3">
+                    <label for="">Course</label>
+                    <input type="text" name="course" class="form-control"/>
+                </div>
+                <div class="form-group">
+                <label for="">Password</label>
+                <input type="password" class="form-control" name="password"/>
+            </div>
+            <div class="form-group">
+            <label for="">Repeat Password</label>
+                <input type="password" class="form-control" name="repeat_password"/>
+            </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Save Student</button>
+            </div>
+        </form>
+        </div>
+    </div>
+</div>
+
+<!-- Edit Student Modal -->
+<div class="modal fade" id="studentEditModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Edit Student</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <form id="updateStudent">
+            <div class="modal-body">
+
+                <div id="errorMessageUpdate" class="alert alert-warning d-none"></div>
+
+                <input type="hidden" name="student_id" id="student_id" >
+
+                <div class="mb-3">
+                    <label for="">full ame</label>
+                    <input type="text" name="name" id="name" class="form-control" />
+                </div>
+                <div class="mb-3">
+                    <label for="">Email</label>
+                    <input type="text" name="email" id="email" class="form-control" />
+                </div>
+                <div class="mb-3">
+                    <label for="">Phone</label>
+                    <input type="text" name="phone" id="phone" class="form-control" />
+                </div>
+                <div class="mb-3">
+                    <label for="">Course</label>
+                    <input type="text" name="course" id="course" class="form-control" />
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary">Update Student</button>
+            </div>
+        </form>
+        </div>
+    </div>
+</div>
+
+<!-- View Student Modal -->
+<div class="modal fade" id="studentViewModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">View Student</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+            <div class="modal-body">
+
+                <div class="mb-3">
+                    <label for="">full name</label>
+                    <p id="view_name" class="form-control"></p>
+                </div>
+                <div class="mb-3">
+                    <label for="">Email</label>
+                    <p id="view_email" class="form-control"></p>
+                </div>
+                <div class="mb-3">
+                    <label for="">Phone</label>
+                    <p id="view_phone" class="form-control"></p>
+                </div>
+                <div class="mb-3">
+                    <label for="">Course</label>
+                    <p id="view_course" class="form-control"></p>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="container mt-4">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4>PHP Ajax CRUD 
+                        
+                        <button type="button" class="btn btn-primary float-end" data-bs-toggle="modal" data-bs-target="#studentAddModal">
+                            Add Student
+                        </button>
+                    </h4>
+                </div>
+                <div class="card-body">
+
+                    <table id="myTable" class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>full name</th>
+                                <th>Email</th>
+                                <th>Phone</th>
+                                <th>Course</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            require 'dbcon.php';
+
+                            $query = "SELECT * FROM registration";
+                            $query_run = mysqli_query($con, $query);
+
+                            if(mysqli_num_rows($query_run) > 0)
+                            {
+                                foreach($query_run as $student)
+                                {
+                                    ?>
+                                    <tr>
+                                        <td><?= $student['id'] ?></td>
+                                        <td><?= $student['full_name'] ?></td>
+                                        <td><?= $student['email'] ?></td>
+                                        <td><?= $student['phone'] ?></td>
+                                        <td><?= $student['course'] ?></td>
+                                        <td>
+                                            <button type="button" value="<?=$student['id'];?>" class="viewStudentBtn btn btn-info btn-sm">View</button>
+                                            <button type="button" value="<?=$student['id'];?>" class="editStudentBtn btn btn-success btn-sm">Edit</button>
+                                            <button type="button" value="<?=$student['id'];?>" class="deleteStudentBtn btn btn-danger btn-sm">Delete</button>
+                                        </td>
+                                    </tr>
+                                    <?php
+                                }
+                            }
+                            ?>
+                            
+                        </tbody>
+                    </table>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
+
+    <script>
+        $(document).on('submit', '#saveStudent', function (e) {
+            e.preventDefault();
+
+            var formData = new FormData(this);
+            formData.append("save_student", true);
+
+            $.ajax({
+                type: "POST",
+                url: "code.php",
+                data: formData,
+                processData: false,
+                contentType: false,
+                success: function (response) {
+                    
+                    var res = jQuery.parseJSON(response);
+                    if(res.status == 422) {
+                        $('#errorMessage').removeClass('d-none');
+                        $('#errorMessage').text(res.message);
+
+                    }else if(res.status == 200){
+
+                        $('#errorMessage').addClass('d-none');
+                        $('#studentAddModal').modal('hide');
+                        $('#saveStudent')[0].reset();
+
+                        alertify.set('notifier','position', 'top-right');
+                        alertify.success(res.message);
+
+                        $('#myTable').load(location.href + " #myTable");
+
+                    }else if(res.status == 500) {
+                        alert(res.message);
+                    }
+                }
+            });
+
+        });
+
+        $(document).on('click', '.editStudentBtn', function () {
+
+            var student_id = $(this).val();
+            
+            $.ajax({
+                type: "GET",
+                url: "code.php?student_id=" + student_id,
+                success: function (response) {
+
+                    var res = jQuery.parseJSON(response);
+                    if(res.status == 404) {
+
+                        alert(res.message);
+                    }else if(res.status == 200){
+
+                        $('#student_id').val(res.data.id);
+                        $('#Name').val(res.data.name);
+                        $('#email').val(res.data.email);
+                        $('#phone').val(res.data.phone);
+                        $('#course').val(res.data.course);
+                        $('#password').val(res.data.password);
+                        $('#repeat_password').val(res.data.repeat_password);
+
+                        $('#studentEditModal').modal('show');
+                    }
+
+                }
+            });
+
+        });
+
+        $(document).on('submit', '#updateStudent', function (e) {
+            e.preventDefault();
+
+            var formData = new FormData(this);
+            formData.append("update_student", true);
+
+            $.ajax({
+                type: "POST",
+                url: "code.php",
+                data: formData,
+                processData: false,
+                contentType: false,
+                success: function (response) {
+                    
+                    var res = jQuery.parseJSON(response);
+                    if(res.status == 422) {
+                        $('#errorMessageUpdate').removeClass('d-none');
+                        $('#errorMessageUpdate').text(res.message);
+
+                    }else if(res.status == 200){
+
+                        $('#errorMessageUpdate').addClass('d-none');
+
+                        alertify.set('notifier','position', 'top-right');
+                        alertify.success(res.message);
+                        
+                        $('#studentEditModal').modal('hide');
+                        $('#updateStudent')[0].reset();
+
+                        $('#myTable').load(location.href + " #myTable");
+
+                    }else if(res.status == 500) {
+                        alert(res.message);
+                    }
+                }
+            });
+
+        });
+
+        $(document).on('click', '.viewStudentBtn', function () {
+
+            var student_id = $(this).val();
+            $.ajax({
+                type: "GET",
+                url: "code.php?student_id=" + student_id,
+                success: function (response) {
+
+                    var res = jQuery.parseJSON(response);
+                    if(res.status == 404) {
+
+                        alert(res.message);
+                    }else if(res.status == 200){
+
+                        $('#view_name').text(res.data.name);
+                        $('#view_email').text(res.data.email);
+                        $('#view_phone').text(res.data.phone);
+                        $('#view_course').text(res.data.course);
+
+                        $('#studentViewModal').modal('show');
+                    }
+                }
+            });
+        });
+
+        $(document).on('click', '.deleteStudentBtn', function (e) {
+            e.preventDefault();
+
+            if(confirm('Are you sure you want to delete this data?'))
+            {
+                var student_id = $(this).val();
+                $.ajax({
+                    type: "POST",
+                    url: "code.php",
+                    data: {
+                        'delete_student': true,
+                        'student_id': student_id
+                    },
+                    success: function (response) {
+
+                        var res = jQuery.parseJSON(response);
+                        if(res.status == 500) {
+
+                            alert(res.message);
+                        }else{
+                            alertify.set('notifier','position', 'top-right');
+                            alertify.success(res.message);
+
+                            $('#myTable').load(location.href + " #myTable");
+                        }
+                    }
+                });
+            }
+        });
+
+    </script>
+</body>
+</html>
